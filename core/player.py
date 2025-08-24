a5173697202adfacb9f5280c549ba9c927a27c8d
Abi Santoni <als.palacios@alumno.um.edu.ar>
2025-08-23 23:03:41 -0300

    Clase Jugador con Tests

---
+++ core/player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, nombre,ficha ):
+        self.__nombre__ = nombre
+        self.__ficha__ = ficha
+
+    def __str__(self):
+        return f"Jugador: {self.__nombre__}, Ficha: {self.__ficha__}"
