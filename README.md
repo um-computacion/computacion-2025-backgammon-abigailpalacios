23fcbb68510633caa8a9390bd547e62f3efacd3f
Abi Santoni <als.palacios@alumno.um.edu.ar>
2025-08-24 21:42:36 -0300

    Clase Dice

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Carrera: Ingenieria en Informática
 
 1er commit (22/08): realizo el primer commit subiendo la estructura del proyecto con las carpetas y archivos necesarios
 2do commit (23/08): Realizo segundo commit con la estructura del jugador y su respectivos tests
+3er commit (24/08): Realizo segundo commit con la estructura de la clase "dado" y su test correspondiente
---
+++ core/dice.py
@@ -0,0 +1,18 @@
+import random
+
+class Dice:
+    def __init__(self):
+        self.__dice1__ = 0
+        self.__dice2__ = 0
+        self.__movimiento__ = []
+
+    def tirar(self):
+        self.__dice1__ = random.randint(1,6)
+        self.__dice2__ = random.randint(1,6)
+        if self.__dice1__ == self.__dice2__:
+            self.__movimiento__ = [self.__dice1__, self.__dice1__, self.__dice2__, self.__dice2__]
+        else:
+            self.__movimiento__ = [self.__dice1__, self.__dice2__]
+        
+    def get_movimiento(self):
+        return self.__movimiento__
